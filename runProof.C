834b4a6a21c48eeb39552849bb88766d4ec44788
Antonio Mendiola <arm160230@utdallas.edu>
2017-11-07 01:24:09 -0600

    Implemented multi-file chaining to runProof.

--- runProof.C
+++ runProof.C
@@ -1,13 +1,23 @@
-void runProof(const char* selector = "", int workers = 20, int chainLen = 1){
+#include <stdarg.h>
+void runProof(const char* selector = "", int workers = 20, int chainLen = 0, ...){
    char processCommand[strlen(selector) + 1];
    strcpy(processCommand, selector);
    strcat(processCommand, "+");
    char numWorkers[10];
    sprintf(numWorkers, "workers=%d", workers);
    TChain ch("ntp1");
-   for(int i = 0; i < chainLen; i++){
+   if(chainLen == 0){
       ch.Add("/net/g20/ph/1/jsn150130/LambdaC-Run1-OffPeak-R24c-v07-1.root");
    }
+   else{
+      va_list fileList;
+      va_start(fileList, chainLen);
+      for(int i = 0; i < chainLen; ++i){
+         //printf("%s\n", va_arg(fileList, const char*));
+         ch.Add(va_arg(fileList, const char*));
+      }
+   }
+   //printf("%lld\n", ch.GetEntries());
    TProof *proof = TProof::Open("lite://", numWorkers);
    ch.SetProof(proof);
    ch.Process(processCommand);
